4b16e08c00eb6a0ac681131728480c9b80749600
artur <artur@vis-design.com>
2015-08-04 17:32:15 +0300

    init

---
+++ src/Vis/Viewed/ViewedServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Vis\Viewed;
+
+use Illuminate\Support\ServiceProvider;
+
+class ViewedServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('vis/viewed');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
---
+++ src/config/config222.php
@@ -0,0 +1,6 @@
+<?php
+
+return array(
+    'time_cookie' => "100000",
+    'limit_count' => "20",
+);
