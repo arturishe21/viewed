52d5cad99bfd8ece0744be48d4db15739b887700
artur <artur@vis-design.com>
2015-09-17 14:11:18 +0300

    rafactoring

--- src/models/Viewed.php
+++ src/models/Viewed.php
@@ -5,32 +5,30 @@ use Illuminate\Support\Facades\Cookie;
 
 class Viewed
 {
-    public function setViewed($class)
+    public function setViewed($model)
     {
-        $nameClass = strtolower(get_class($class));
+        $nameClass = strtolower(get_class($model));
 
-        if (isset($class->id) && $class->id) {
-            $idPage = $class->id;
+        if (isset($model->id) && $model->id) {
+            $idPage = $model->id;
             $nameCookie = md5($nameClass . "_viewed");
             $cookies = unserialize(Cookie::get($nameCookie));
 
             if (is_array($cookies)) {
                 array_unshift($cookies, $idPage);
-                array_slice($cookies, 0,
-                    Config::get("viewed.config.limit_count"));
+                array_slice($cookies, 0, Config::get("viewed.config.limit_count"));
             } else {
                 $cookies[] = $idPage;
             }
 
             $cookies = array_unique($cookies);
-            Cookie::queue($nameCookie, serialize($cookies),
-                Config::get("viewed::config.time_cookie"));
+            Cookie::queue($nameCookie, serialize($cookies), Config::get("viewed::config.time_cookie"));
         }
     }
 
-    public function getViewed($class)
+    public function getViewed($model)
     {
-        $nameClass = strtolower(get_class($class));
+        $nameClass = strtolower(get_class($model));
         $nameCookie = md5($nameClass . "_viewed");
         $cookies = unserialize(Cookie::get($nameCookie));
 
